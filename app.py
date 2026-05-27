5c71cc5a8b68f28f56cebd7ce89b3c001cd232ca
santanaXAXA <arturahalikov@gmail.com>
2026-05-27 23:07:57 +0300

    remove telegram bot

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from analyzer import analyze, rewrite
-from bot import start_bot_thread
 
 app = Flask(__name__)
-start_bot_thread()
 
 
 @app.route("/")
